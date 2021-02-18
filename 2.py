c1168ae05772065f99589c5001bbecbc64a837ed
kisellovf <rodion230806@bk.ru>
2021-02-18 19:52:26 +0300

    homework

---
+++ 2.py
@@ -0,0 +1,8 @@
+fn = input("Первое число: ")
+sn = input("Второе число: ")
+tn = input("Третье число: ")
+if float(fn) % float(tn) == 0:
+    print(float(fn) / float(tn))
+
+else:
+    print(float(fn) % float(sn))
